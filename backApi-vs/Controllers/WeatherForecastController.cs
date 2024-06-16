69aae08bb22229557fd54ab4291063a07dbc1c3b
PruebasF <20181165@uthh.edu.mx>
2024-06-15 20:20:48 -0600

    cambios

--- backApi-vs/Controllers/WeatherForecastController.cs
+++ backApi-vs/Controllers/WeatherForecastController.cs
@@ -11,7 +11,7 @@ namespace backApi_vs.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Prueba","Tina","tempor"
         };
-
+        //tina cambios
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly IRepositorio repositorio;
 
