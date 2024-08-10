f081c898105c9089d0a577ba453d043733bfa3ed
faustino01 <20181165@uthh.edu.mx>
2024-08-10 17:52:45 -0600

    Reglas de validacion definidas

--- backApi-vs/Entidades/Genero.cs
+++ backApi-vs/Entidades/Genero.cs
@@ -5,7 +5,16 @@ namespace backApi_vs.Entidades
     public class Genero
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength:10)]
         public string Nombre { get; set; } = string.Empty;
+        [Range(18,120)]
+        public int Edad {  get; set; }
+        [CreditCard]
+        public string TarjetaDeCredito { get; set; } = string.Empty;
+        [Url]
+        public string Url {  get; set; }  = string.Empty;
+
+
     }
 }
