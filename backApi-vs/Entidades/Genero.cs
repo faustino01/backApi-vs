d2dcb6dd55543431970cff39978a519561d2764f
faustino01 <20181165@uthh.edu.mx>
2024-08-10 18:53:15 -0600

    Validacion por Modelo

--- backApi-vs/Entidades/Genero.cs
+++ backApi-vs/Entidades/Genero.cs
@@ -3,12 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace backApi_vs.Entidades
 {
-    public class Genero
+    public class Genero:IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength:10)]
-        [PrimeraLetraMayuscula]
+        // [PrimeraLetraMayuscula]
         public string Nombre { get; set; } = string.Empty;
         [Range(18,120)]
         public int Edad {  get; set; }
@@ -17,6 +17,15 @@ namespace backApi_vs.Entidades
         [Url]
         public string Url {  get; set; }  = string.Empty;
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            //throw new NotImplementedException();
+            var primeraLetra = Nombre[0].ToString();
 
+            if (primeraLetra != primeraLetra.ToUpper())
+            {
+                yield return new ValidationResult("La primeta letra debe de ser mayuscula", new string[] { nameof(Nombre) });
+            }
+        }
     }
 }
