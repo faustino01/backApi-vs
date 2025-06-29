a138f0fd8a74db9ecb846b697dc4cfff50d34d63
faustino01 <20181165@uthh.edu.mx>
2025-06-29 10:06:14 -0600

    post registro bd post primer registro real

--- backApi-vs/Controllers/GenerosController.cs
+++ backApi-vs/Controllers/GenerosController.cs
@@ -5,7 +5,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 using System.Data;
+using System.Threading.Tasks;
 
 
 namespace backApi_vs.Controllers
@@ -16,29 +18,35 @@ namespace backApi_vs.Controllers
     public class GenerosController:Controller
     {
         private readonly ILogger<GenerosController> logger;
+        private readonly ApplicationDbContext context;
 
-        public GenerosController(ILogger<GenerosController> logger) {
+        public GenerosController(ILogger<GenerosController> logger, ApplicationDbContext context) {
             this.logger = logger;
+            this.context = context;
         }
         [HttpGet]
       
-        public ActionResult<List<Genero>> Get()
+        public async Task<ActionResult<List<Genero>>> Get()
         {
-            return new List<Genero>() { new Genero() { Id = 1, Nombre = "TINA TIN" } };
+           // return new List<Genero>() { new Genero() { Id = 1, Nombre = "TINA TIN" } };
+           return await context.Generos.ToListAsync();
         } 
       
         //se puede validar el tipo de parametro a recibir Id:int de esta manera se limita a solo tipo numerico
         [HttpGet("{Id:int}")]
         public async Task<ActionResult<Genero?>> Get(int Id) 
         {
-           throw new NotImplementedException();
+              throw new NotImplementedException();
+           
         }
 
         [HttpPost]
-        public ActionResult Post([FromBody] Genero genero)
+        public async Task<ActionResult> Post([FromBody] Genero genero)
         {
-            throw new NotImplementedException();
-
+            //throw new NotImplementedException();
+            context.Add(genero);
+            await context.SaveChangesAsync();
+            return NoContent();
         }
         [HttpPut]
         public ActionResult Put([FromBody] Genero genero)
